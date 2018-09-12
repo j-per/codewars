09e58853b3659f345af8fc2b1ca041099daa3193
j-per <perdue.jesse@gmail.com>
2018-09-12 12:31:09 -0700

    Add files via upload

---
+++ arithmetic/arithmetic.js
@@ -0,0 +1,22 @@
+function arithmetic(num1, num2, operator) {
+    switch(operator) {
+        case "multiply":
+            console.log(num1 * num2);
+            break;
+
+        case "add":
+            console.log(num1 + num2);
+            break;
+
+        case "subtract":
+            console.log(num1 - num2);
+            break;
+
+        case "divide":
+            console.log(num1 / num2);
+            break;
+    }
+}
+
+arithmetic(5, 5, "subtract");
+
